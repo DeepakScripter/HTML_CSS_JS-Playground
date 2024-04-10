e4148338ba0ddff4d844ed5db5ff91328e48aa80
Deepak Vishwakarma <coder.deepak44@gmail.com>
2024-04-10 18:39:18 +0530

    News App  📰📱

---
+++ News App 📰📱/README.md
@@ -0,0 +1,32 @@
+# 📰 News App
+
+## Overview
+📰 This is a simple news application that allows users to fetch news articles based on different categories like IPL, Finance, Politics, etc. Users can also search for news articles using keywords.
+
+## Technologies Used
+🛠️ - HTML
+🎨 - CSS
+💻 - JavaScript
+
+## Features
+✨ - Fetch news articles from an external API (NewsAPI)
+📷 - Display news articles with images, titles, sources, and descriptions
+🔍 - Navigate between different news categories
+🔎 - Search for news articles using keywords
+📱 - Responsive design for mobile and desktop devices
+
+
+## 📸 Screenshots
+
+![News App](assets/Screenshot%202024-04-08%20134206.png)
+
+## Setup Instructions
+1. Clone the repository to your local machine.
+2. Open the project folder in your code editor.
+3. Open the `index.html` file in your web browser or start a local development server.
+4. Use the navigation links to browse different news categories or search for news articles using keywords.
+
+## Credits
+- [NewsAPI](https://newsapi.org/) for providing the news data.
+- Icons used in the project are from [FontAwesome](https://fontawesome.com/).
+- Fonts used in the project are from [Google Fonts](https://fonts.google.com/).
---
+++ News App 📰📱/script.js
@@ -0,0 +1,85 @@
+// Declaring constants for API key and base URL
+const API_KEY = "8da7c8a5da504a1d8e577db5a439087e";
+const url = "https://newsapi.org/v2/everything?q=";
+
+// Event listener for loading the page, fetching news with default query "India"
+window.addEventListener("load", () => fetchNews("India"));
+
+// Function to reload the page
+function reload() {
+  window.location.reload();
+}
+
+// Asynchronous function to fetch news based on a query
+async function fetchNews(query) {
+  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+  const data = await res.json();
+  bindData(data.articles);
+}
+
+// Function to bind fetched news data to HTML
+function bindData(articles) {
+  const cardsContainer = document.getElementById("cards-container");
+  const newsCardTemplate = document.getElementById("template-news-card");
+
+  cardsContainer.innerHTML = "";
+
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, article);
+    cardsContainer.appendChild(cardClone);
+  });
+}
+
+// Function to fill data in news card template
+function fillDataInCard(cardClone, article) {
+  const newsImg = cardClone.querySelector("#news-img");
+  const newsTitle = cardClone.querySelector("#news-title");
+  const newsSource = cardClone.querySelector("#news-source");
+  const newsDesc = cardClone.querySelector("#news-desc");
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank");
+  });
+}
+
+// Variable to keep track of the currently selected navigation item
+let curSelectedNav = null;
+
+// Function to handle click event on navigation items
+function onNavItemClick(id) {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = navItem;
+  curSelectedNav.classList.add("active");
+}
+
+// Event listener for search button click
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+  const query = searchText.value;
+  if (!query) return;
+  fetchNews(query);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = null;
+});
+var a = 10;
+var b = 1;
+var c = a-b;
+
+console.log(a);
+console.log(c);
